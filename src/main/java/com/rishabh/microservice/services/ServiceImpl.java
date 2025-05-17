7c8a8eef85efba918b9f4face573bf8e7e185b5f
rishabh9102000 <rishabh.chaurasia2000@gmail.com>
2025-05-18 00:27:48 +0530

    added logs and upload artifacts

--- src/main/java/com/rishabh/microservice/services/ServiceImpl.java
+++ src/main/java/com/rishabh/microservice/services/ServiceImpl.java
@@ -2,20 +2,26 @@ package com.rishabh.microservice.services;
 
 import com.rishabh.microservice.pojo.Userss;
 import com.rishabh.microservice.repo.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+
 @Component
 
 public class ServiceImpl implements Servicess {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceImpl.class);
 
     @Autowired
     UserRepo userRepo;
 
     @Override
     public Userss addUser(String name, String message) {
+        logger.info("Add user service called");
         Userss use = new Userss(name,message);
         System.out.println(use);
         return userRepo.save(use);
@@ -23,7 +29,7 @@ public class ServiceImpl implements Servicess {
 
     @Override
     public Userss getUser(String name) {
-
+        logger.info("getUser service called");
         Userss use = userRepo.findByName(name);
         System.out.println(use);
         return use;
@@ -31,6 +37,7 @@ public class ServiceImpl implements Servicess {
 
     @Override
     public List<Userss> getAll() {
+        logger.info("getALL service callled");
         return  userRepo.findAll();
     }
 }
